04dde4359a50886fef3486c0018251f4b466276a
MichaelWiss <michael.wiss@gmail.com>
2024-10-10 15:47:33 -0500

    functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,27 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function subtract () {
+
+}
+
+function multiply () {
+
+}
+
+function divide () {
+    
+}
+
+addBtn.addEventListener('click', add);
